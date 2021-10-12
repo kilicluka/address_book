55a8c5c1e4771623d2dca8260c937db7644e1783
Luka Kilic <kilic.luka@gmail.com>
2021-10-12 15:36:01 +0200

    Add Addres and UserAddress admin pages

--- src/address/admin.py
+++ src/address/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from address.models import Address, UserAddress
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'country', 'state', 'city', 'zip_code', 'address_one', 'created_at', 'updated_at')
+    list_filter = ('country', 'state', 'city', 'zip_code')
+    search_fields = ('zip_code', 'address_one')
+    readonly_fields = ('created_at', 'updated_at')
+
+
+admin.site.register(Address, AddressAdmin)
+
+
+class UserAddressAdmin(admin.ModelAdmin):
+    list_display = ('user', 'address', 'additional_address_data', 'created_at', 'updated_at')
+    search_fields = ('user', 'address')
+    readonly_fields = ('created_at', 'updated_at')
+
+
+admin.site.register(UserAddress, UserAddressAdmin)
